00c74a9d9fd8a448ca6a9c2cc8d79e33d10b630a
Jennifer Wang <jlw2219@columbia.edu>
2015-04-12 15:09:38 -0400

    Added a div for Read More, changed the name of the data in the for loop (made a mistake before.)

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
-for (var i=0; i<guardianData.length; i++) {
-	var rowData = guardianData[i]
+for (var i=0; i<guardianHeroinData.length; i++) {
+	var rowData = guardianHeroinData[i];
 	
-	var template = '<div class="quoteContainer">'+
-				'<div class="mainQuote">'+rowdata.mainQuote+'</div>'+
-				'<div class="wholeQuote">'+rowdata.wholeQuote+'</div>'+
-				'<div class="source">'+rowData.source+'</div>'+
-				'<div class="yearsClean">'+rowData.yearsClean+'</div>'+
-				'<div class="location">'+rowData.location+'</div>'+
-			'</div>'+
+	var $template = $('<div class="quoteContainer">'+ // put $ in front of var template as a remind that it's JQuery-fied.
+				'<div class="mainQuote">'+rowData.mainQuote+'</div>'+
+				'<div class="readMore">Read More</div>'+
+				'<div class="moreInfo">'+
+					'<div class="wholeQuote">'+rowData.wholeQuote+'</div>'+
+					'<div class="source">'+rowData.source+'</div>'+
+					'<div class="yearsClean">'+rowData.yearsClean+'</div>'+
+					'<div class="location">'+rowData.location+'</div>'+
+				'</div>'+
+			'</div>');
+			
+	$template.find(".readMore").on("click", toggleContent);
+			
+	$('#dataContainer').append($template);
 			
 }
+
